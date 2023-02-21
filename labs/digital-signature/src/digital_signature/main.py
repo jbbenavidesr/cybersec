d3186a413d3e2593adfc0f7bbdd501bc3a644485
Juan Benavides <jbbenavidesr@gmail.com>
2023-02-21 13:54:07 +0000

    :sparkles: Add a first working version of the digital signature

--- labs/digital-signature/src/digital_signature/main.py
+++ labs/digital-signature/src/digital_signature/main.py
@@ -1,27 +1,56 @@
+"""Simple program to sign and verify a file."""
+from pathlib import Path
+
+from nacl.encoding import Base64Encoder, Encoder, RawEncoder
 from nacl.signing import SigningKey, VerifyKey
 
 
-def main():
+def sign(
+    message: bytes, signing_key: SigningKey, encoder: Encoder = RawEncoder
+) -> bytes:
+    """Sign a message with a given signing key."""
+    signed = signing_key.sign(message, encoder=encoder)
+    return signed.signature
+
+
+def verify(message: bytes, signature: bytes, verify_key: VerifyKey) -> bool:
+    """Verify a message with a given verify key."""
+    try:
+        verify_key.verify(message, signature)
+        return True
+    except Exception:
+        return False
+
+
+def main(file_name: Path) -> None:
+    """Entrypoint to digital signature program."""
     # Generate a new signing key
     signing_key = SigningKey.generate()
-
-    # Sign a message with the signing key
-    signed = signing_key.sign(b"Message")
-
-    # Obtain the verify key for a given signing key
     verify_key = signing_key.verify_key
+    verify_key_bytes = verify_key.encode(encoder=Base64Encoder)
 
-    # Serialize the verify key to send it to a third party
-    verify_key_bytes = verify_key.encode()
+    with open(file_name, "rb") as f:
+        message = f.read()
 
-    # Create a verify key from the hex string
-    verify_key = VerifyKey(verify_key_bytes)
+    signed = sign(message, signing_key, encoder=Base64Encoder)
 
-    verify_key.verify(signed)
+    output_file_name = file_name.with_suffix(".sig")
+    verivy_key_file_name = file_name.with_suffix(".key")
 
-    print(signed.message)
-    print(signed.signature)
+    with open(output_file_name, "wb") as f:
+        f.write(signed)
+
+    with open(verivy_key_file_name, "wb") as f:
+        f.write(verify_key_bytes)
+
+    print(f"Signed file: {output_file_name}")
 
 
 if __name__ == "__main__":
-    main()
+    import sys
+
+    if len(sys.argv) != 2:
+        print("Usage: python main.py <file_name>")
+        sys.exit(1)
+    path = Path(sys.argv[1])
+    main(path)
